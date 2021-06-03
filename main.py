eb5fd839175a0578271bce1c68f6bcb4063b730c
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2021-06-03 22:27:42 +0300

    created dataset with nlp metrics and author's style metrics

--- main.py
+++ main.py
@@ -1,37 +1,66 @@
 # -*- coding: utf-8 -*-
+from nltk.util import pr
 import numpy 
 import nltk
 import glob
 import os
 import string
+from array import *
+import csv
 
-#read file for learning
+#array with file`s index
+filesIndex = ['11', '12', '13', '14', '15', '16', '17', '18',
+            '19', '110', '111', '01', '02', '03', '04', '05', '06',
+            '07', '08', '09', '010']
 
-lesFile = open('database\LesyaUkrainkaKonvalia.txt','r', encoding='utf-8')
-firstText = lesFile.read()
-#print(firstText)
-lesFile.close()
-firstText = firstText.replace("’",'').replace("‘", '').replace("’", '').replace("'", '').replace("–", ' ')
+#create csv file with dataset for analitics
 
-sergioFile = open('database\SergioZhadanSchoTiBudeshZhaduvati.txt','r', encoding='utf-8')
-secondText = sergioFile.read()
-#print(secondText)
-sergioFile.close()
-secondText = secondText.replace("’",'').replace("‘", '').replace("’", '').replace("'", '').replace("–", ' ')
+open('datasetWithMetrics.csv', 'w').close()
+for file in filesIndex :
 
-#tokenization
-nltk.download('punkt')
+    #reading start files
 
-tokensFirst = nltk.word_tokenize(firstText)
-#print(tokensFirst)
+    nameFile = "database\\"+file+".txt" 
+    print(nameFile)
+    fileForClean = open(nameFile,'r', encoding='utf-8')
+    textForClean = fileForClean.read()
+    fileForClean.close()
+    textForClean = textForClean.replace("’",'').replace("‘", '').replace("’", '').replace("'", '').replace("–", ' ').replace("«", ' ').replace("»", ' ')
 
-remove_punctuation = str.maketrans('', '', string.punctuation)
-tokens_first = [x for x in [t.translate(remove_punctuation).lower() for t in tokensFirst] if len(x) > 0]
-print(tokens_first)
+    #tokenize
+    nltk.download('punkt')
 
-tokensSecond = nltk.word_tokenize(secondText)
-#print(tokensSecond)
+    tokensFile = nltk.word_tokenize(textForClean)
+    #print(tokensFile)
 
-remove_punctuation = str.maketrans('', '', string.punctuation)
-tokens_second = [x for x in [t.translate(remove_punctuation).lower() for t in tokensSecond] if len(x) > 0]
-print(tokens_second)
+    delPunctuation = str.maketrans('', '', string.punctuation)
+    tokensWithoutPunct = [x for x in [token.translate(delPunctuation).lower() 
+                                for token in tokensFile]
+                                if len(x) > 0]
+    print(tokensWithoutPunct)
+
+    #defining author's style metrics
+
+    #lexical diversity of the text
+
+    textDev = nltk.Text(tokensWithoutPunct)
+    lexicalDiversity = (len(set(textDev)) / len(textDev)) * 100 
+    print("Lexical diversity : "+str(lexicalDiversity))
+
+    #average lenght of word
+
+    words = set(tokensWithoutPunct)
+    charNum = [len(word) for word in words]
+    averageNumChar = sum(charNum) / float(len(charNum))
+    print("Average length words : " + str(averageNumChar))
+
+    #average length of sentence
+
+    sentencesList = nltk.sent_tokenize(textForClean)
+    wordNum = [len(sent.split()) for sent in sentencesList]
+    averageNumWord = numpy.mean(wordNum)
+    print("Average length sentences : "+str(averageNumWord))
+
+    with open('datasetWithMetrics.csv', 'a', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter='~', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow([file, lexicalDiversity, averageNumChar, averageNumWord, file[0] ])
